b43aaa3a817bca8301bf1e27424a18af7d71ea3f
Pepper <1994097635@qq.com>
2019-04-08 12:02:17 +0800

    添加了 cors 的 SpringBoot 的配置

---
+++ cors/CorsConfiger.java
@@ -0,0 +1,26 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfiger {
+	private CorsConfiguration buildCorsConfiger() {
+		CorsConfiguration corsc = new CorsConfiguration();
+		corsc.addAllowedOrigin("*");
+		corsc.addAllowedMethod("*");
+		corsc.addAllowedHeader("*");
+		corsc.addExposedHeader("*");
+		return corsc;
+	}
+	
+	//配置
+	@Bean
+	public CorsFilter getCorsFilter() {
+		UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
+		configSource.registerCorsConfiguration("/**", buildCorsConfiger());
+		CorsFilter corsf = new CorsFilter(configSource );
+		return corsf;
+	}
+}
